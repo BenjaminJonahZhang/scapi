e1ed50489a51373c59d240cc7d4f41057b1d81b9
Asaf Kleinbort <asaf.kleinbort@gmail.com>
2015-08-11 16:24:54 +0300

    compiling on windows work now with boost and with Catch for testing

--- src/cpp/ScapiCpp/include/Dlog.hpp
+++ src/cpp/ScapiCpp/include/Dlog.hpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <boost/multiprecision/cpp_int.hpp>
+#include <boost/timer.hpp>
 using namespace std;
 
 class DlogGroup
@@ -7,5 +8,6 @@ class DlogGroup
 public:
 	virtual string getGroupType();
 	virtual boost::multiprecision::int1024_t getOrder();
+	virtual boost::timer some();
 	DlogGroup();
 };
