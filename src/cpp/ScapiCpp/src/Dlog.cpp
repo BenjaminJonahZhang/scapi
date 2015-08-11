bb0d8e4b88061341a49f7d8108e11842f8e09a12
Asaf Kleinbort <asaf.kleinbort@gmail.com>
2015-08-11 15:24:53 +0300

    Starting to work Dlog

---
+++ src/cpp/ScapiCpp/src/Dlog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "../include/Dlog.hpp"
+
+using namespace std;
+
+string DlogGroup::getGroupType()
+{
+	return "no no";
+}
+
+DlogGroup::DlogGroup(void)
+{
+	cout << "creating Group";
+}
---
+++ src/cpp/ScapiCpp/src/tests.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "../include/catch.hpp"
+#include "../include/Dlog.hpp"
+
+TEST_CASE("Factorials are computed", "[factorial]") {
+	DlogGroup dg;
+	REQUIRE(dg.getGroupType() == "no no");
+}
