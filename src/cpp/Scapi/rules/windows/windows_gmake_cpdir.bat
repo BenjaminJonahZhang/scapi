f305a2c0b0fee4c2e536628a5f72f55e6314a059
AsafKleinbort <asaf.kleinbort@gmail.com>
2015-10-06 14:54:29 +0300

    fixing merge

---
+++ src/cpp/Scapi/rules/windows/windows_gmake_cpdir.bat
@@ -0,0 +1,27 @@
+@echo off
+rem  This file is copyrighted, see copyright.txt
+
+setlocal
+
+if not "%:~2%" == "" goto doit
+
+:error
+
+echo windows_gmake_cp.bat:  error:  expected exactly 2 parameters
+goto done
+
+:doit
+    
+if not "%3%4%5%6%8%9" == "" goto error
+
+set input1=%1
+set output1=%2
+
+set input=%input1:/=\%
+set output=%output1:/=\%
+
+xcopy /q /i /e /y %input% %output%
+
+
+
+:done
