0ddbf54722d525586b80903431374f59299c5eba
Asaf Kleinbort <asaf.kleinbort@gmail.com>
2015-11-10 09:55:43 +0200

    Adding test for communication setup - Native and non-native

--- src/java/edu/biu/scapi/tests/comm/TestScCbcMacPrepending.java
+++ src/java/edu/biu/scapi/tests/comm/TestScCbcMacPrepending.java
@@ -1,21 +1,41 @@
 package edu.biu.scapi.tests.comm;
 
-import org.junit.Test;	
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.security.InvalidKeyException;
-
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
-
 import edu.biu.scapi.comm.AuthenticatedChannel;
+import edu.biu.scapi.comm.Channel;
+import edu.biu.scapi.comm.CommunicationSetup;
+import edu.biu.scapi.comm.ConnectivitySuccessVerifier;
+import edu.biu.scapi.comm.LoadParties;
+import edu.biu.scapi.comm.NaiveSuccess;
+import edu.biu.scapi.comm.Party;
 import edu.biu.scapi.comm.PlainChannel;
+import edu.biu.scapi.comm.twoPartyComm.NativeSocketCommunicationSetup;
+import edu.biu.scapi.comm.twoPartyComm.PartyData;
 import edu.biu.scapi.comm.twoPartyComm.PlainTCPSocketChannel;
+import edu.biu.scapi.comm.twoPartyComm.SocketPartyData;
+import edu.biu.scapi.comm.twoPartyComm.TwoPartyCommunicationSetup;
+import edu.biu.scapi.exceptions.DuplicatePartyException;
 import edu.biu.scapi.exceptions.InvalidChannelException;
 import edu.biu.scapi.exceptions.SecurityLevelException;
 import edu.biu.scapi.midLayer.symmetricCrypto.mac.Mac;
 import edu.biu.scapi.midLayer.symmetricCrypto.mac.ScCbcMacPrepending;
 import edu.biu.scapi.primitives.prf.bc.BcAES;
+import junit.framework.AssertionFailedError;
 
 
 public class TestScCbcMacPrepending {
@@ -52,4 +72,74 @@ public class TestScCbcMacPrepending {
 		PlainTCPSocketChannel  ptsc = new PlainTCPSocketChannel(); // create not initialized channel 
 		createAuthenticatedChannel(ptsc);  // non-initialized channel raises exception
 	}
+	
+	private Channel setCommunication(SocketPartyData partyMe, SocketPartyData partyOther) {
+		TwoPartyCommunicationSetup commSetup = null;
+		try {
+			commSetup = new NativeSocketCommunicationSetup(partyMe, partyOther);
+			Map<String, Channel> connections = null;
+			connections = commSetup.prepareForCommunication(1, 200000);
+			return (Channel)((connections.values()).toArray())[0];			
+		} catch (DuplicatePartyException | TimeoutException e1) {
+			e1.printStackTrace();
+			return null;
+		}
+	}
+	
+	private Channel setCommunicationNotNative(Party party0, Party party1)
+	{
+		List<Party> listOfParties = new LinkedList<Party>(Arrays.asList(party0, party1));
+		// Create the communication setup.
+		CommunicationSetup commSetup = new CommunicationSetup();
+		ConnectivitySuccessVerifier naive = new NaiveSuccess();
+		System.out.print("Before call to prepare\n");
+		Map<InetSocketAddress, Channel> connections = commSetup.prepareForCommunication(listOfParties, naive, 200000);
+		//Return the channel with the other party. There was only one channel created.
+		return (Channel)((connections.values()).toArray())[0];
+	}
+	
+	private void testChannel(Channel c, String dataToSend, String expectedDataToRcv) {
+		  try{
+			  c.send(dataToSend);
+			  String res0 = (String) c.receive();
+			  assertEquals(expectedDataToRcv, res0);
+			  c.close();
+		  }
+		  catch(IOException | ClassNotFoundException e){
+			  fail(e.getMessage());
+		  }
+	}
+	
+	@Test
+	public void TestNativeConnectionSanity() throws UnknownHostException{
+		InetAddress ip = InetAddress.getByName("127.0.0.1");
+		SocketPartyData party0 = new SocketPartyData(ip, 25001);
+		SocketPartyData party1 = new SocketPartyData(ip, 25002);
+		String dataFrom0To1 = "Hi 1 this is 0";
+		String dataFrom1To0 = "Hi 0 this is 1";
+		(new Thread() {
+			  public void run() {
+				  Channel c0 = setCommunication(party0, party1);
+				  testChannel(c0, dataFrom0To1 , dataFrom1To0);
+			  }}).start();
+		Channel c1 = setCommunication(party1, party0);
+		testChannel(c1, dataFrom1To0, dataFrom0To1);
+	}
+	
+	@Test
+	public void TestNonNativeConnectionSanity() throws UnknownHostException{
+		InetAddress ip = InetAddress.getByName("127.0.0.1");
+		Party party0 = new Party(ip, 25003);
+		Party party1 = new Party(ip, 25004);
+		String dataFrom0To1 = "Hi 1 this is 0";
+		String dataFrom1To0 = "Hi 0 this is 1";
+		(new Thread() {
+			  public void run() {
+				  Channel c0 = setCommunicationNotNative(party0, party1);
+				  testChannel(c0, dataFrom0To1 , dataFrom1To0);
+			  }}).start();
+		Channel c1 = setCommunicationNotNative(party1, party0);
+		testChannel(c1, dataFrom1To0, dataFrom0To1);
+	}
 }
+
