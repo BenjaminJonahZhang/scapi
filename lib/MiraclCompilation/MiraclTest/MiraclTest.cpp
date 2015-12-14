e26b5a134845953dda4754b94e17d54466d99b91
Asaf Kleinbort <asaf.kleinbort@gmail.com>
2015-12-14 13:14:55 +0200

    adding miracl windows solution to allow easy build of win64 cpp version for miracl

---
+++ lib/MiraclCompilation/MiraclTest/MiraclTest.cpp
@@ -0,0 +1,12 @@
+// MiraclTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../MiraclWin64/source/big.h"
+
+int main()
+{
+	miracl *mip = mirsys(163, 2);
+    return 0;
+}
+
