02cd5899dcaaa4864e90f110edca994108eb61cb
liorbiu <liork.cryptobiu@gmail.com>
2017-07-20 15:26:04 +0300

    add -force flag to the delete images and container

--- rebuild.sh
+++ rebuild.sh
@@ -2,8 +2,8 @@ git submodule init
 git submodule update
 
 # delete all existing containers
-docker ps -a -q | xargs --no-run-if-empty docker rm
+docker ps -a -q | xargs --no-run-if-empty docker rm -f
 # delete all images
-docker images -q | xargs --no-run-if-empty docker rmi
+docker images -q | xargs --no-run-if-empty docker rmi -f
 # build a fresh one
 docker build --no-cache -t scapicryptobiu/scapi -f dockerfiles/DockerfileBuild .
